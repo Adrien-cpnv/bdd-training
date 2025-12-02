d7f7e5ac17ecc1379c35a10496d5e0ee35e3acc0
pw10thu <pw10thu@eduvaud.ch>
2025-12-02 14:00:04 +0100

    Feat: llGetters_NominalCase_Success and price_setPriceNominalCase_Success done and valid

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -22,41 +22,41 @@ module.exports = class CartItem {
     }
 
     get articleId() {
-        //TODO Implement this method
+        return this.#articleId;
     }
 
     get name() {
-        //TODO Implement this method
+        return this.#name;
     }
 
     get quantity() {
-        //TODO Implement this method
+       return this.#quantity;
     }
 
     set quantity(value) {
-        //TODO Implement this method
+        this.#quantity = value;
     }
 
     get price() {
-        //TODO Implement this method
+        return this.#price;
     }
 
     set price(value) {
-        //TODO Implement this method
+        this.#price = value;
     }
 
     get total() {
-        //TODO Implement this method
+        return this.#quantity * this.#price;
     }
     //endregion public methods
 
     //region private methods
-    set #articleId(value) {
-        //TODO Implement this method
+    set articleId(value) {
+        this.#articleId = value;
     }
 
-    set #name(value) {
-        //TODO Implement this method
+    set name(value) {
+        this.#name = value;
     }
 
     #validateArticleId(articleId) {
